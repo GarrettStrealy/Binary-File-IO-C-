285052ee5e6d8cb1681a8e1ea623f0ef63d79c91
GarrettStrealy <gstrealy@gmail.com>
2020-11-26 01:54:46 -0600

    completed project

---
+++ hw6.h
@@ -0,0 +1,15 @@
+/*
+ * File:                hw6.h
+ * Date:                11/21/2020
+ * Author:              Garrett Strealy
+ * Email:               garrett.strealy@utdallas.edu
+ * Version:             1.0
+ * Copyright:           2020, All Rights Reserved
+ *
+ * Description:
+ *
+ *  2 classes
+ */ 
+
+class BinaryFileHeader() { }
+class BinaryFileRecord() { }
